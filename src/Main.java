0819d8fea071301d61ab63a1c0d1da59a594cdcc
Roman Varanita <romanvaranita@gmail.com>
2023-03-02 20:09:37 +0100

    Add getSeason method

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,14 @@ public class Main {
     SUMMER,
     FALL,
   }
+  public static Season getSeason(String month) {
+    return switch (month.toLowerCase() ) {
+     case "dec", "jan", "feb", -> Season.WINTER;
+     case "mar", "apr", "may", -> Season.SPRING;
+     case "jun", "jul",
+    }
+  }
+
   public static void main(String[] args) {
     System.out.println("Hello world!");
 
